977defb113b4d181d26238c6863c7fe8e019f506
Olav Markussen <olav@hammer.studby.ntnu.no>
2015-03-11 02:48:18 +0100

    Add notifications for group and appointment
    Closes #31
    Closes #32

---
+++ server/api/appointmentNotification.controller.js
@@ -0,0 +1,69 @@
+module.exports = function(app){
+
+	var AppointmentNotification = require('../models/appointmentNotificaiton.model')(app);
+
+	return {
+		createAppointmentNotification: function(req, res) {
+			AppointmentNotification.forge({
+				Message: req.body.message,
+				Viewstatus: req.body.viewstatus,
+				Offset: req.body.offset
+			}).save()
+			.then(function(notification) {
+				res.send(notification.toJSON());
+			})
+			.catch(function(err) {
+				return res.send(500, {error:err.toString()});
+			});
+		},
+
+		getAppointmenNotification: function(req, res) {
+			new AppointmentNotification({apppointmentNotificationID: req.params.apppointmentNotificationID}).fetch()
+			.then(function(appointmentNotification) {
+				if(!appointmentNotification) return res.json({error: 'appointmentNotification not found'});
+				res.send(appointmentNotification.toJSON());
+			})
+			.catch(function(err) {
+				return res.send(500, {error: err.toString()});
+			});
+		},
+
+		updateAppointmentNotification: function(req, res) {
+			new AppointmentNotification({apppointmentNotificationID: req.params.appointmentNotificationID}).fetch()
+			.then(function(appointmentNotification) {
+				if(!appointmentNotificaiton) return res.json(400, {error: 'appointmentNotification not found'});
+				appointmentNotificaiton.save({
+					Message: req.body.message || appointmentNotificaiton.get('Message'),
+					Viewstatus: req.body.viewstatus || appointmentNotificaiton.get('Viewstatus')
+					Offset: req.body.offset || appointmentNotification.get('Offset')
+				})
+				.then(function(updatenotification) {
+					res.send(updateAppointmentNotification.toJSON())
+				})
+				.catch(function(err) {
+					return res.send(500, {error: err.toString()});
+				});
+			})
+			.catch(function(err) {
+				return res.send(500, {error: err.toString()});
+			});
+		},
+
+		deleteAppointmentNotification: function(req, res) {
+			new AppointmentNotification({apppointmentNotificationID: req.params.apppointmentNotificationID}).fetch()
+			.then(function(appointmentNotification) {
+				if(!appointmentNotificaiton) return res.json(400, {error: 'appointmentNotificaiton not found'});
+				appointmentNotificaiton.destroy()
+				.then(function() {
+					return res.send(200);
+				})
+				.catch(function(err) {
+					return res.send(500, {error: err.toString()});
+				});
+			})
+			.catch(function(err) {
+				return res.send(500, {error: err});
+			});
+		}
+	}
+}
